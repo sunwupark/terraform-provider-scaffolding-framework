838f078e1f63853d1532a08876e41e4651ca5915
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-06-23 18:25:09 +0900

    Fix: Delete error files

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -14,9 +14,9 @@ import (
 // acceptance testing. The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
-var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
-}
+// var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+// 	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
+// }
 
 func testAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
